6b605987301e3b8239bc14c9c0fb280f8ca99af4
alic-xc <aladelekan187@gmail.com>
2019-07-01 10:40:03 +0100

    #007 - updating the urls with some views

--- apps/urls.py
+++ apps/urls.py
@@ -1,9 +1,21 @@
 from django.urls import path
-from .views import merge_view, tagging_view, remove_view
+from .views import (
+        index_view,
+        login_view, 
+        merge_view, 
+        profile_view,
+        remove_view, 
+        register_view, 
+        tagging_view, 
+    )
 
 
 urlpatterns = [
-    path('merge', merge_view, name='merge' ),
-    path('tagging', tagging_view, name = 'tagging'),
-    path('remove', remove_view, name='remove')
+    path('', index_view, name='index'),
+    path('apps/merge', merge_view, name='merge' ),
+    path('apps/tagging', tagging_view, name = 'tagging'),
+    path('apps/remove', remove_view, name='remove'),
+    path('user/register', register_view, name='create'),
+    path('user/signin', login_view, name='login'),
+    path('user/<uuid>/profile',profile_view, name='profile' )
 ]
