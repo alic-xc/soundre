bdba7d953b026913ecff637c988290fc0252af6e
alic-xc <aladelekan187@gmail.com>
2019-07-02 00:20:01 +0100

    #013 - removing redundant codes and update its views

--- apps/urls.py
+++ apps/urls.py
@@ -1,21 +1,25 @@
 from django.urls import path
 from .views import (
+        dashboard_view,
         index_view,
-        login_view, 
+        login_view,
+        logout_view,
         merge_view, 
         profile_view,
         remove_view, 
         register_view, 
-        tagging_view, 
+        tagging_view,
     )
 
 
 urlpatterns = [
     path('', index_view, name='index'),
-    path('apps/merge', merge_view, name='merge' ),
-    path('apps/tagging', tagging_view, name = 'tagging'),
-    path('apps/remove', remove_view, name='remove'),
-    path('user/register', register_view, name='create'),
-    path('user/signin', login_view, name='login'),
-    path('user/<uuid>/profile',profile_view, name='profile' )
+    path('dashboard', dashboard_view, name='dashboard'),
+    path('merge', merge_view, name='merge' ),
+    path('tagging', tagging_view, name='tagging'),
+    path('remove', remove_view, name='remove'),
+    path('register', register_view, name='create'),
+    path('login', login_view, name='login'),
+    path('logout', logout_view, name='logout'),
+    path('profile', profile_view, name='profile' )
 ]
