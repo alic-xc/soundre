15ff836de530839206749c84f2e448555f866632
alic-xc <aladelekan187@gmail.com>
2019-07-01 10:41:33 +0100

    #008 - importing audioEditing, forms modules

--- apps/views.py
+++ apps/views.py
@@ -1,13 +1,42 @@
-from django.http import HttpResponse
 from django.shortcuts import render
-
+from .forms import TagViewForm, TagEditForm
+from audioEditing.tagging import Tagging
 # Create your views here.
 
-def merge_view(request):
-    return HttpResponse(" <H1>  merge 2 audio together")
 
-def tagging_view(request):
-    return HttpResponse("<H2> tagging a mp3 <H2>")
+def index_view(request):
+    # get all tags associated with the file
+    audio_form = TagViewForm()
+    context = {
+        "audio": audio_form,
+        "audio_tags": None,
+    }
+
+    if request.method == "POST":
+        audio_form = TagViewForm(request.POST, request.FILES)
+
+        if audio_form.is_valid() is True:
+            # Do something
+            tags = Tagging(request.FILES['audio'])
+            tags.view_tags()
+
+    return render(request, "home/index.html", context)
+
+
+def merge_view(request):
+    pass
 
 def remove_view(request):
-    return HttpResponse("<h3> remove audio <h3>")
+    pass
+
+def tagging_view(request):
+    pass
+
+def register_view(request):
+    pass
+
+def login_view(request):
+    pass
+
+def profile_view(request):
+    pass
