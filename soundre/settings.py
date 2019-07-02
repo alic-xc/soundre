1eb2b5b4137377c4557c2c3afb12c0b556e09177
alic-xc <aladelekan187@gmail.com>
2019-07-03 00:55:32 +0100

    #016 - added login and media urls

--- soundre/settings.py
+++ soundre/settings.py
@@ -123,4 +123,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+LOGIN_URL = '/login/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 STATIC_URL = '/static/'
