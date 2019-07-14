333e01f7bd2faf06f0b5c4164dde1c1804cd3948
alic-xc <aladelekan187@gmail.com>
2019-07-14 18:38:17 +0100

    update settings

--- soundre/settings.py
+++ soundre/settings.py
@@ -133,4 +133,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 django_heroku.settings(locals())
+
