d4cf7bb8ca7ca7510ee92c3952f250b03b60192a
alade lekan <aladelekan187@gmail.com>
2021-03-03 17:15:19 +0100

    modal for audio lists

--- account/views.py
+++ account/views.py
@@ -1,10 +1,12 @@
 from django.contrib import messages
 from django.contrib.auth import logout, authenticate, login
 from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
 from django.shortcuts import redirect
 from django.urls import reverse
 from django.views import generic
-from account.forms import RegistrationForm, LoginForm
+from account.forms import RegistrationForm, LoginForm, UploadAudioForm
+from account.models import AudioModel
 
 
 class RegistrationView(generic.FormView):
@@ -52,9 +54,43 @@ class ProfileView(generic.TemplateView):
     template_name = 'account/profile.html'
 
 
-class DashboardView(generic.TemplateView):
+class DashboardView(generic.FormView):
     """ User Dashboard """
     template_name = 'account/dashboard.html'
+    form_class = UploadAudioForm
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        context['audio_objects'] = AudioModel.objects.filter(user=self.request.user)
+        return context
+
+    def get_success_url(self):
+        return reverse('dashboard')
+
+    def form_valid(self, form):
+        total_files = AudioModel.objects.filter(user=self.request.user).count()
+
+        # Check total files uploaded by user
+        if total_files > 5:
+            messages.error(self.request, "Max upload limit reached.")
+            return super().form_invalid(form)
+
+        form.instance.user = self.request.user
+        form.save()
+        messages.success(self.request, "Audio added successfully.")
+        return super().form_valid(form)
+
+
+@login_required
+def fetch_audio(request):
+    """ A AJAX request to pull all audio for a user """
+    total_files = AudioModel.objects.filter(user=request.user)
+    data = []
+    for file in total_files:
+        data.append({'id': file.hash, 'name': file.name, 'file': file.audio.url})
+    return JsonResponse(data={'data': data}, status=200)
+
+
 
 @login_required
 def logout_view(request):
