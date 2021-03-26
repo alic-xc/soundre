04d57f6001bf426594f72b1ab129624f0170dd7d
alade lekan <aladelekan187@gmail.com>
2021-03-26 21:05:36 +0100

    cleanup: remove some unwanted codes

--- apps/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import AudioModel, CoverPictureModel
-# Register your models here.
-
-admin.site.register(AudioModel)
-admin.site.register(CoverPictureModel)
