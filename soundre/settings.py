2ad41b16c0c732d6ae4d4a96fa85d3b4e60a140d
alade lekan <aladelekan187@gmail.com>
2021-03-27 17:12:19 +0100

    fix: edit config for ALLOWED_HOST

--- soundre/settings.py
+++ soundre/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = config('ALLOWED_HOST', cast=Csv(int))
+ALLOWED_HOSTS = config('ALLOWED_HOST', default='*', cast=Csv())
 
 
 # Application definition
